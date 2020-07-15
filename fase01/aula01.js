3d912526ef83ea4c0b36b96368c2d9c8437e7527
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-07-15 18:37:08 -0300

    Começando o curso

---
+++ fase01/aula01.js
@@ -0,0 +1,15 @@
+// Criar um programa que calcula a média 
+// das notas entre os alunos e envia
+// mensagem do cálculo da média.
+
+const aluno01 = "Henrique";
+const aluno02 = 'Mayk';
+const aluno03 = `Fulano`;
+
+const notaAluno01 = 9.8;
+const notaAluno02 = 10;
+const notaAluno03 = 2;
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3;
+
+console.log(media.toFixed(2));

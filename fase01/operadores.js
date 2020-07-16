ff2c8b5d03b01495b10b7b33428d9827e8033241
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-07-16 00:36:09 -0300

    operadores

---
+++ fase01/operadores.js
@@ -0,0 +1,60 @@
+// Operadores Relacionais
+// >    Maior que
+// <    Menor que
+// >=   Maior igual á
+// <=   Menor igual á
+
+/* const a = 5;
+const b = 6;
+console.log(a > b); // false
+console.log(a < b); // true
+console.log(a >= b); // false
+console.log( a <= b); // true */
+
+
+// Operadores de igualdade
+// ==   Igual a
+// !=   Diferente de 
+// ===  Igual e do mesmo tipo
+// !==  Diferente inclusive do tipo
+
+/* const a = 5;
+const b = '5';
+console.log(a == b); // true
+console.log(a === b); // false
+console.log(a != b); // false
+console.log(a !== b); // true */
+
+
+// Operadores Lógicos
+//São tipicamente usados com valores booleanos (true or false)
+//Nos ajudam a criar expressões lógicas apartir de 2 ou mais valores
+// &&   "E" vai comparar dois booleanos e retornar verdadeiro caso AS DUAS expressões sejam verdadeiras
+// ||   "OU" vai comparar dois booleanaos retornar verdadeiro caso UMA DAS expressões sejam verdadeiras 
+// !    "NÃO" vai negar uma expressão
+
+/* const a = 5;
+const b = '5';
+
+console.log(a == b && a !== b); // true 
+console.log(a == b && a != b); // false
+
+console.log(a == b || a !== b); // true 
+console.log(a == b || a != b); // true
+console.log(a != b || a === b); // false
+
+console.log(!(a == b || a != b)); // false
+console.log(!true); //false */
+
+// Operadores Aritméticos
+// *    Multiplicação
+// /    Divisão
+// %    Resto da divisão
+// +    Adição
+// -    Subtração
+
+console.log(2 * 2); // 4
+console.log(2 / 2); // 1
+console.log(2 % 1.4); // 0.6
+console.log(2 + 2); // 4
+console.log(2 - 2); // 0
